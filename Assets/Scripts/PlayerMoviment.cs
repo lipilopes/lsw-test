c14aaf84d3cc1c1e664cce1f84deaf0f175b2c37
lipilopes <felippe091234@gmail.com>
2021-11-10 16:20:08 -0300

    update script player moviment

--- Assets/Scripts/PlayerMoviment.cs
+++ Assets/Scripts/PlayerMoviment.cs
@@ -6,36 +6,44 @@ using UnityEngine;
 public class PlayerMoviment : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 2.5f;
-    //float horizontalMoviment;
-    //float verticalMoviment;
 
     Vector2 movement;
 
     Rigidbody2D rb;
     Animator anim;
 
+    bool canMove = true;
+    public bool CanMove {set{    canMove = value; } }
+
     void Awake() 
     {
-        rb = GetComponent<Rigidbody2D>();
-        anim = GetComponent<Animator>();
+        rb      = GetComponent<Rigidbody2D>();
+        anim    = GetComponent<Animator>();
     }
 
     private void Update() 
     {
-        //horizontalMoviment  = Input.GetAxisRaw("Horizontal");
-        //verticalMoviment    = Input.GetAxisRaw("Vertical");
+        if(canMove)
+        {
+            movement.x  = Input.GetAxisRaw("Horizontal");
+            movement.y  = Input.GetAxisRaw("Vertical"); 
+        }
 
-        movement.x  = Input.GetAxisRaw("Horizontal");
-        movement.y  = Input.GetAxisRaw("Vertical"); 
-
-        anim.SetFloat("Horizontal",movement.x);
-        anim.SetFloat("Vertical",movement.y);
-        anim.SetFloat("Speed",movement.sqrMagnitude);
+        Anim();      
     }
 
     private void FixedUpdate() 
     {
-        //rb.velocity  = new Vector2(horizontalMoviment,verticalMoviment)*moveSpeed;
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    void Anim()
+    {
+        if(anim == null)
+            return;
+
+        anim.SetFloat("Horizontal",movement.x);
+        anim.SetFloat("Vertical",movement.y);
+        anim.SetFloat("Speed",movement.sqrMagnitude);
     }
 }
