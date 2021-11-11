5bc5bc5804021dc930be54fdfa0dbee4b2647de6
lipilopes <felippe091234@gmail.com>
2021-11-10 21:57:04 -0300

    add script mob scriptable

---
+++ Assets/Scripts/Systems/MobScriptable.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Scriptables", menuName = "Scriptable/Mob", order = 1)
+,HelpURL("https://docs.unity3d.com/ScriptReference/Editor.html")]
+public class MobScriptable : ScriptableObject
+{
+    [SerializeField] string _name;
+    public string Name { get { return _name;}}
+
+    [Tooltip("[Idle] - [Smile] - [Talk] - [Angry]")]
+    [SerializeField] Sprite[] _portrait;
+    public Sprite Portrait(PortraitFeeling feeling)
+    {
+        return _portrait[(int)feeling];
+    }
+}
