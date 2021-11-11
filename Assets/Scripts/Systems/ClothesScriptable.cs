962dfb177a5a5cb44f417f2579059ffe94895219
lipilopes <felippe091234@gmail.com>
2021-11-11 15:45:02 -0300

    update script clothes scriptable

--- Assets/Scripts/Systems/ClothesScriptable.cs
+++ Assets/Scripts/Systems/ClothesScriptable.cs
@@ -13,28 +13,34 @@ public enum ClothesEnum
 [CreateAssetMenu(fileName = "", menuName = "Scriptable/Clothes", order = 1)]
 public class ClothesScriptable : ScriptableObject
 {
-    [SerializeField] string _name;
-    public string Name { get { return _name;}}
+    [SerializeField]
+    private string _name;
+    public  string Name { get { return _name; } }
 
-    [SerializeField] Sprite _portraitSprite;
-    public Sprite PortraitSprite { get { return _portraitSprite;}}
+    [SerializeField]
+    private Sprite _portraitSprite;
+    public  Sprite PortraitSprite { get { return _portraitSprite; } }
 
-    [SerializeField] ClothesEnum _clothesType;
-    public ClothesEnum ClothesType { get { return _clothesType;}}
+    [SerializeField]
+    private ClothesEnum _clothesType;
+    public  ClothesEnum ClothesType { get { return _clothesType; } }
 
     // [Tooltip("Shop Icon")]
     // [SerializeField] Sprite _iconSprite;
     // public Sprite IconSprite { get { return _iconSprite;}}
 
     [Header("Color")]
-    [SerializeField] bool _isMultColor;
-    public bool isMultColor { get { return _isMultColor;}}
+    [SerializeField]
+    private bool _isMultColor;
+    public  bool isMultColor { get { return _isMultColor; } }
 
-    [SerializeField] Color _color;
-    public Color GetColor { get { return _isMultColor ? _color : Color.white;}}
+    [SerializeField]
+    private Color _color;
+    public  Color GetColor { get { return _isMultColor ? _color : Color.white; } }
     [Space]
     [Tooltip("Name of OverrideController in Resources folder")]
-    [SerializeField] string ControllerName;
+    [SerializeField]
+    private string ControllerName;
 
     /// <summary>
     /// animator.runtimeAnimatorController = Resources.Load<RuntimeAnimatorController>(ControllerPath);
