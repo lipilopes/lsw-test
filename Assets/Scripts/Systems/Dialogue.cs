e7260a9681132d4f12db4dd59240a2b16425c365
lipilopes <felippe091234@gmail.com>
2021-11-11 15:45:27 -0300

    update script dialogue

--- Assets/Scripts/Systems/Dialogue.cs
+++ Assets/Scripts/Systems/Dialogue.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 [System.Serializable]
 public class Dialogue
-{
-    public bool             playerSpeaking = false;
-    public PortraitFeeling  portraitFeeling;
-    public Emotions         emotionBallon = Emotions.None;
-    [TextArea]
+{   [TextArea]
     public string           text;
+    public bool             playerSpeaking          = false;
+    [Header("Portraits")]
+    public bool             showNpcPortrait         = true;
+    public PortraitFeeling  portraitFeeling;
+    [Space]
+    public bool             showPlayerPortrait      = true;
+    public PortraitFeeling  playerPortraitFeeling;
+    [Header("Balloons")]
+    public Emotions         emotionBalloon          = Emotions.None;
+    public Emotions         playerEmotionBalloon    = Emotions.None;
+
 }
