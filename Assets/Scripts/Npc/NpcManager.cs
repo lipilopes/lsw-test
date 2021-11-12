efab0924ce2509e65e2413f961178223fb02e5b5
lipilopes <felippe091234@gmail.com>
2021-11-12 05:18:46 -0300

    upgrade script npc manager

--- Assets/Scripts/Npc/NpcManager.cs
+++ Assets/Scripts/Npc/NpcManager.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D),typeof(MobsClothes))]
 public class NpcManager : MonoBehaviour
 {
+    #if UNITY_EDITOR
+    Vector3 _respaw = Vector3.zero;
+    Vector3 _test   = new Vector3(4.1f,-1.31f,0);
+    void TESTENPC()
+    {
+        if(_respaw  == Vector3.zero)
+            _respaw = this.transform.position;
+
+        if(this.transform.position != _respaw)
+            this.transform.position = _respaw;
+        else
+            this.transform.position = _test;
+    }
+
+    [ContextMenuItem("Test NPC", "TESTENPC")]
+    #endif
     [SerializeField] 
     private NpcScriptable scriptable;
     public  NpcScriptable Scriptable { get{ return scriptable;} }
