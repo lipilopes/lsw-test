962064016418f38cc4bcbee026876045014f3ad7
lipilopes <felippe091234@gmail.com>
2021-11-12 05:19:40 -0300

    upgrade script npc scritable

--- Assets/Scripts/Npc/NpcScriptable.cs
+++ Assets/Scripts/Npc/NpcScriptable.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable] 
+public enum NpcType
+{
+    None,
+    Speaker,
+    Shopkeeper//dialog + shop
+}
+
 [CreateAssetMenu(fileName = "Scriptables/Npc", menuName = "Scriptable/Npc", order = 1)]
 public class NpcScriptable : MobScriptable
 {
+    [Header("NPC")]
+    [SerializeField]
+    private NpcType _npcType;
+    public  NpcType Type { get { return _npcType;}}
+
+    [Header("Speaker")]
     [SerializeField]
     private Dialogue[] _dialogues;
     public  Dialogue[] Dialogues { get { return _dialogues;}}
@@ -13,4 +27,9 @@ public class NpcScriptable : MobScriptable
     {
         return _portrait[(int)Dialogues[dialogueIndex].portraitFeeling];
     }
+
+    [Header("Shopkeeper")]
+    [SerializeField]
+    private List<ClothesScriptable> storeList = new List<ClothesScriptable>();
+    public  List<ClothesScriptable> StoreList { get { return storeList; } }
 }
